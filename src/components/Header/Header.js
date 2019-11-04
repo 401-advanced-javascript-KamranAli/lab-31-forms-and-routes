9737d4a4f61287efdb99963d16b1af6988ba8bc3
Kamran Ali <kgali21@gmail.com>
2019-11-04 14:31:24 -0800

    added a header component, no nav

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Header = () => (
+  <>
+    <header>
+      <h1>Avatar: The Last Airbender</h1>
+    </header>
+  </>
+);
+
+export default Header;
