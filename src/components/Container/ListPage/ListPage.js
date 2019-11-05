e278754aece380d7f635a4272a9b95e4710f116c
Kamran Ali <kgali21@gmail.com>
2019-11-04 16:24:16 -0800

    created list page and card

--- src/components/Container/ListPage/ListPage.js
+++ src/components/Container/ListPage/ListPage.js
@@ -1,6 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import ListCard from '../../ListCard/ListCard';
 
-export default class ListPage extends Component {
-  
-}
+const List = ({ list }) => {
+  const listElements = list.map(listItem => (
+    <li key={`${listItem.image}-${listItem.name}`}>
+      <ListCard {...listItem} />
+    </li>
+  ));
+
+  return (
+    <>
+      <h1>Check these characters out!</h1>
+      <ul>
+        {listElements}
+      </ul>
+    </>
+  );
+};
+
+List.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.shape({
+    image: PropTypes.string,
+    url: PropTypes.string
+  }))
+};
+
+export default List;
